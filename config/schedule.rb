0b71ffdee5c5e9f13bafb0c975ddb4dfae9d2947
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2022-07-14 15:28:26 +0300

    schelude

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,9 +6,9 @@ every 1.day, :at => '04:00' do
   runner "ImportInsalesXmlJob.perform_later"
 end
 
-every 1.day, :at => '14:30' do
+every 1.day, :at => '16:00' do
   runner "CreateCsvPriceJob.perform_later"
 end
-every 1.day, :at => '14:40' do
+every 1.day, :at => '16:05' do
   runner "CreateCsvQuantityJob.perform_later"
 end
