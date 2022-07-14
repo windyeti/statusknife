ee50d69dedbe88556379432d58c1f6c2cea2fb56
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2022-07-14 21:24:57 +0300

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "dashboards#index"
+  root to: "products#index"
   devise_for :users, controllers: {registrations: "registrations"}
 
   resources :products do
