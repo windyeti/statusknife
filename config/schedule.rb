aee837385b6aa65c0ec60971b6aa483385cc2d39
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2024-01-10 17:14:05 +0300

    add 3 time parsing

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,3 +13,37 @@ end
 every 1.day, :at => '05:00' do
   runner "ApiInsalesPriceJob.perform_later"
 end
+
+# -------------------
+every 1.day, :at => '09:00' do
+  runner "DpoUpdateJob.perform_later"
+end
+
+every 1.day, :at => '10:00' do
+  runner "ImportInsalesXmlJob.perform_later"
+end
+
+every 1.day, :at => '11:00' do
+  runner "ApiInsalesQuantityJob.perform_later"
+end
+
+every 1.day, :at => '12:00' do
+  runner "ApiInsalesPriceJob.perform_later"
+end
+
+# -------------------
+every 1.day, :at => '16:00' do
+  runner "DpoUpdateJob.perform_later"
+end
+
+every 1.day, :at => '17:00' do
+  runner "ImportInsalesXmlJob.perform_later"
+end
+
+every 1.day, :at => '18:00' do
+  runner "ApiInsalesQuantityJob.perform_later"
+end
+
+every 1.day, :at => '19:00' do
+  runner "ApiInsalesPriceJob.perform_later"
+end
