619f8ddcd251527fc14db39f214f87a3e1c2c615
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2025-03-04 13:28:21 +0300

    chore check size log

--- app/services/util/check_size_log.rb
+++ app/services/util/check_size_log.rb
@@ -2,7 +2,9 @@ class Services::Util::CheckSizeLog
   def call
     ["delayed_job.log", "production.log"].each do |file_name|
       path = File.join("/var/www/statusknife/shared/log", file_name)
-      File.rm(path) if File.exist?(path) && File.size(path) > 10000000
+      if File.exist?(path) && File.size(path) > 100000000
+        File.open(path, 'w') {|file| file.truncate(0) }
+      end
     end
   end
 end
