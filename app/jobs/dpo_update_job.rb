e8c95d063cd29affc6d13605ae10b0d03cc62f36
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2026-08-28 11:47:04 +0300

    comm cabel

--- app/jobs/dpo_update_job.rb
+++ app/jobs/dpo_update_job.rb
@@ -2,8 +2,8 @@ class DpoUpdateJob < ApplicationJob
   queue_as :default
 
   def perform
-    ActionCable.server.broadcast 'status_process', {distributor: "dpo", process: "update_distributor", status: "start", message: "Обновление товаров поставщика Dpo"}
+    # ActionCable.server.broadcast 'status_process', {distributor: "dpo", process: "update_distributor", status: "start", message: "Обновление товаров поставщика Dpo"}
     Services::Vendor::DpoUpdate.new.call
-    ActionCable.server.broadcast 'status_process', {distributor: "dpo", process: "update_distributor", status: "finish", message: "Обновление товаров поставщика Dpo"}
+    # ActionCable.server.broadcast 'status_process', {distributor: "dpo", process: "update_distributor", status: "finish", message: "Обновление товаров поставщика Dpo"}
   end
 end
