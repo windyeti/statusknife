17b8deb0cc37ffca7b8a2956c924281c79ebc020
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2026-09-02 23:51:43 +0300

    full schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -15,41 +15,40 @@ every 1.day, :at => '05:00' do
 end
 
 # -------------------
-# every 1.day, :at => '09:00' do
-#   runner "DpoUpdateJob.perform_later"
-# end
-#
-# every 1.day, :at => '10:50' do
-#   runner "ImportInsalesXmlJob.perform_later"
-# end
-#
-# every 1.day, :at => '11:00' do
-#   runner "ApiInsalesQuantityJob.perform_later"
-# end
-#
-# every 1.day, :at => '12:00' do
-#   runner "ApiInsalesPriceJob.perform_later"
-# end
+every 1.day, :at => '09:00' do
+  runner "DpoUpdateJob.perform_later"
+end
+
+every 1.day, :at => '10:50' do
+  runner "ImportInsalesXmlJob.perform_later"
+end
+
+every 1.day, :at => '11:00' do
+  runner "ApiInsalesQuantityJob.perform_later"
+end
+
+every 1.day, :at => '12:00' do
+  runner "ApiInsalesPriceJob.perform_later"
+end
 
 # ------------------- Check Size Log
-#
 every 1.day, :at => '13:00' do
   runner "CheckSizeLogJob.perform_later"
 end
 
 # # -------------------
-# every 1.day, :at => '16:00' do
-#   runner "DpoUpdateJob.perform_later"
-# end
-#
-# every 1.day, :at => '17:50' do
-#   runner "ImportInsalesXmlJob.perform_later"
-# end
-#
-# every 1.day, :at => '18:00' do
-#   runner "ApiInsalesQuantityJob.perform_later"
-# end
-#
-# every 1.day, :at => '19:00' do
-#   runner "ApiInsalesPriceJob.perform_later"
-# end
+every 1.day, :at => '16:00' do
+  runner "DpoUpdateJob.perform_later"
+end
+
+every 1.day, :at => '17:50' do
+  runner "ImportInsalesXmlJob.perform_later"
+end
+
+every 1.day, :at => '18:00' do
+  runner "ApiInsalesQuantityJob.perform_later"
+end
+
+every 1.day, :at => '19:00' do
+  runner "ApiInsalesPriceJob.perform_later"
+end
